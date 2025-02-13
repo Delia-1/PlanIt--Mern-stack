f26471b15f54e10da061977fd2f944d20e655db6
DÃelia Knoepfli <delia.knoepfli@gmail.com>
2025-02-13 15:33:42 +0100

    server.js

--- backend/server.js
+++ backend/server.js
@@ -22,5 +22,5 @@ import todoRoutes from './routes/todoRoutes.js';
 app.use('/api/todos', todoRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running port ${PORT}`);
 });
